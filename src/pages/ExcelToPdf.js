4f42fb29053d616018bad32a316bcd45ac6bb5e3
Yug Korat <yughkorat@gmail.com>
2026-01-28 14:35:34 +0530

    update

--- src/pages/ExcelToPdf.js
+++ src/pages/ExcelToPdf.js
@@ -13,7 +13,7 @@ const ExcelToPdf = () => {
         formData.append('file', file);
 
         try {
-            const res = await axios.post('https://api.pdfeditor.live/office-to-pdf'
+            const res = await axios.post('https://api.pdfeditor.live/excel-to-pdf'
 , formData, { responseType: 'blob' });
             const url = window.URL.createObjectURL(new Blob([res.data]));
             const link = document.createElement('a');
